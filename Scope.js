96a954888929ebfe26b5333e3b93d276874d47b5
Asif-24 <khondokerasif78@gmail.com>
2021-12-18 21:56:17 +0600

    asdf=

---
+++ Scope.js
@@ -0,0 +1,11 @@
+let bonous = 23;
+
+function sum(first,second){
+    let result = first + second + bonous;
+    console.log(result);
+
+    return result;
+}
+
+const output = sum(234,33)
+console.log(output);
